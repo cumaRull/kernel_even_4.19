7c204d1f3394289b820f176822aef15dfd4759b6
ZYLEX <86944731+Badmaneers@users.noreply.github.com>
2025-01-29 10:42:09 +0000

    fix: update Ptr2UINT32 macro to use uintptr_t for pointer conversion

--- drivers/misc/mediatek/sensor/2.0/oplus_sensor_devinfo/sensor_devinfo.c
+++ drivers/misc/mediatek/sensor/2.0/oplus_sensor_devinfo/sensor_devinfo.c
@@ -3,6 +3,8 @@
  * Copyright (C) 2018-2020 Oplus. All rights reserved.
  */
 #include <linux/uaccess.h>
+#include <linux/types.h>
+#include <linux/stddef.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
@@ -67,7 +69,7 @@ enum {
 #define DEVINFO_LOG(fmt, args...)   pr_err(DEV_TAG"%s %d : "fmt, __func__, __LINE__, ##args)
 
 #define UINT2Ptr(n)     (uint32_t *)(n)
-#define Ptr2UINT32(p)   (uint32_t)(p)
+#define Ptr2UINT32(p)   ((uint32_t)(uintptr_t)(p))
 #define MAG_PARA_OFFSET               8
 /*#define SOURCE_NUM                    3*/
 #define MAG_PARA_NUM                  9
@@ -851,8 +853,8 @@ static int als_cali_read_func(struct seq_file *s, void *v)
 {
 	void *p = s->private;
 
-	DEVINFO_LOG("Ptr2UINT32(p) = %d \n", Ptr2UINT32(p));
-	switch (Ptr2UINT32(p)) {
+	DEVINFO_LOG("((uint32_t)(uintptr_t)(p)) = %d \n", ((uint32_t)(uintptr_t)(p)));
+	switch (((uint32_t)(uintptr_t)(p))) {
 	case RED_MAX_LUX:
 		seq_printf(s, "%d", gdata->red_max_lux);
 		break;
@@ -885,7 +887,7 @@ static ssize_t als_cali_write(struct file *filp, const char *ubuf, size_t cnt, l
 	int ret = 0;
 	struct seq_file *s = filp->private_data;
 	void *p = s->private;
-	int node = Ptr2UINT32(p);
+	int node = ((uint32_t)(uintptr_t)(p));
 
 	if (cnt >= sizeof(buf)) {
 		return -EINVAL;
@@ -1513,7 +1515,7 @@ int get_msensor_parameter(int num)
 	return 0;
 }
 
-void  mag_soft_parameter_init()
+void  mag_soft_parameter_init(void)
 {
 	int ret = -1;
 	int index = 0;
@@ -1534,8 +1536,8 @@ static int sensor_feature_read_func(struct seq_file *s, void *v)
 	int ret = 0;
 	int selftest_result = 0;
 
-	DEVINFO_LOG("Ptr2UINT32(p) = %d \n", Ptr2UINT32(p));
-	switch (Ptr2UINT32(p)) {
+	DEVINFO_LOG("((uint32_t)(uintptr_t)(p)) = %d \n", ((uint32_t)(uintptr_t)(p)));
+	switch (((uint32_t)(uintptr_t)(p))) {
 	case IS_SUPPROT_HWCALI:
 		if (!strcmp(sensorlist_info[ps].name, "tcs3701")) {
 			seq_printf(s, "%d", 1);
@@ -1619,7 +1621,7 @@ static ssize_t sensor_feature_write(struct file *filp, const char *ubuf, size_t 
 	int result = 0;
 	struct seq_file *s = filp->private_data;
 	void *p = s->private;
-	int node = Ptr2UINT32(p);
+	int node = ((uint32_t)(uintptr_t)(p));
 
 	if (cnt >= sizeof(buf)) {
 		return -EINVAL;
