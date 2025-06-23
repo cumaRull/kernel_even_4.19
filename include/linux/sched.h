0312d7a4704fa4e13a7dc1752e5cf5cda8cbef52
Cuma.Rull Mods <rulsmods@gmail.com>
2025-06-23 04:11:36 +0000

    Revert "include: linux: fix error on write"
    This reverts commit e0aee46e0184d29ace10cfc89c2e84f82a5c5e7d.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1486,13 +1486,16 @@ struct task_struct {
 #endif
 	struct mutex			futex_exit_mutex;
 #endif
-ANDROID_KABI_RESERVE(7);
-#ifdef CONFIG_KSU_SUSFS
-    ANDROID_KABI_USE(8, u64 susfs_last_fake_mnt_id);
+
+#else
+ 	ANDROID_KABI_RESERVE(7);
+#endif
+
+	#ifdef CONFIG_KSU_SUSFS
+	ANDROID_KABI_USE(8, u64 susfs_last_fake_mnt_id);
 #else
  	ANDROID_KABI_RESERVE(8);
 #endif
-
 #ifdef CONFIG_MTK_TASK_TURBO
 	unsigned short turbo:1;
 	unsigned short render:1;
